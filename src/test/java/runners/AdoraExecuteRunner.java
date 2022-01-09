d6134632d196ffb7e86ed92cea2a0a906ed855f1
RahulJain95 <jnrahul.95@gmail.com>
2022-01-09 08:41:42 +0530

    update

--- src/test/java/runners/AdoraExecuteRunner.java
+++ src/test/java/runners/AdoraExecuteRunner.java
@@ -3,6 +3,6 @@ package runners;
 import core.AutoEngBaseTest;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = {"@BO_Coupons", "not @ignore"})
+@CucumberOptions(tags = {"@Basic_Order_Entry_Dine_In_Cash_With_All_Lines_Enabled", "not @ignore"})
 public class AdoraExecuteRunner extends AutoEngBaseTest {
 }
