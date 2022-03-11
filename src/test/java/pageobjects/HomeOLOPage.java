04fbe766ded6de2864a3a2b0b1e094d6edff8310
RahulJain95 <jnrahul.95@gmail.com>
2022-03-11 22:27:48 +0530

    update code

--- src/test/java/pageobjects/HomeOLOPage.java
+++ src/test/java/pageobjects/HomeOLOPage.java
@@ -13,8 +13,18 @@ public class HomeOLOPage extends BasePO {
     private By hawaiianPizzaP = By.xpath("//button[@aria-label='Hawaiian Pizza (P)']");
     private By chicagoStylePizzaM = By.xpath("//button[@aria-label='Chicago Style Pizza (M)']");
     private By veggiePizzaC = By.xpath("//button[@aria-label='Veggie Pizza (C)']");
+    private By pepperoniPizzaMC = By.xpath("//button[@aria-label='Pepperoni Pizza (M-C)']");
+    private By supremePizzaPM = By.xpath("//button[@aria-label='Supreme Pizza (P-M)']");
     private By cheesePizzaPMC = By.xpath("//button[@aria-label='Cheese Pizza (P-M-C)']");
     private By checkOut = By.id("btn_OB_CheckOut");
+
+    public Element supremePizzaPM() throws IOException, InterruptedException {
+        return $(supremePizzaPM);
+    }
+
+    public Element pepperoniPizzaMC() throws IOException, InterruptedException {
+        return $(pepperoniPizzaMC);
+    }
 
     public Element veggiePizzaC() throws IOException, InterruptedException {
         return $(veggiePizzaC);
