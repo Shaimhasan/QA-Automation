0f44b952d35136f83f344c86e28bf4cfaae8c686
RahulJain95 <jnrahul.95@gmail.com>
2022-09-17 19:42:02 +0530

    update code

--- src/main/java/core/AutoEngBaseTest.java
+++ src/main/java/core/AutoEngBaseTest.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = {"core.AutoEngineFormatter",
                 "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
+                "json:target/cucumber-reports/runReport.json",
                 "rerun:failScenarios/rerun.txt"},
         features = {"classpath:features"}
 )
