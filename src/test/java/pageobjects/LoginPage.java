25fe761cbe106f4a2e03b9dfbfc29aa071d94b6b
RahulJain95 <jnrahul.95@gmail.com>
2023-05-03 18:58:19 +0530

    update the xpath of connect

--- src/test/java/pageobjects/LoginPage.java
+++ src/test/java/pageobjects/LoginPage.java
@@ -12,7 +12,7 @@ public class LoginPage extends BasePO {
     private By employee_Id = By.id("txtEmployeeID");
     private By password = By.id("txtEmployeePassword");
     private By stationKey = By.id("txtStationKey");
-    private By connect = By.xpath("//button[@type='submit']");
+    private By connect = By.xpath("//button[text()='Connect']");
     private By waitTillLoading = By.xpath("//div[text()='Loading Items Availability (32 of 32)... Please wait.']");
     private By continueToLogin = By.xpath("//button[text()='Continue to Login']");
 
