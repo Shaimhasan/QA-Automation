ce2e01de7a1ecde6cfc51cb70a7ad4a17eb6b2f7
RahulJain95 <jnrahul.95@gmail.com>
2023-09-17 11:02:11 +0530

    Changed xpath enterComment in Add and Edit Comments Page

--- src/test/java/pageobjects/AddCommentsPage.java
+++ src/test/java/pageobjects/AddCommentsPage.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 
 public class AddCommentsPage extends BasePO {
 
-    private By enterComment = By.id("txt_BO_MN_COM_ADDEDT_Comment_en-us");
-    private By enterCommentsSpanish = By.id("txt_BO_MN_COM_ADDEDT_Comment_sp-mx");
+    private By enterComment = By.id("txt_BO_MN_COM_ADDEDT_Comment_en-US");
+    private By enterCommentsSpanish = By.id("txt_BO_MN_COM_ADDEDT_Comment_es-MX");
     private By save = By.xpath("//button[@onclick='BO_MN_COM_ADDEDT_Save();']");
     private By commentsTextPopUp = By.xpath("//div[@id='div_BO_MN_COM_ADDEDT_DIALOG_modal_view']//span[text()='Comment']");
 
