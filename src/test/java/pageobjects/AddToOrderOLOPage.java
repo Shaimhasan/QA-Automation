f4ded829daf68a512428a0e663f62efc6dabead3
RahulJain95 <jnrahul.95@gmail.com>
2023-09-26 07:42:56 +0530

    add size xpath

--- src/test/java/pageobjects/AddToOrderOLOPage.java
+++ src/test/java/pageobjects/AddToOrderOLOPage.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 public class AddToOrderOLOPage extends BasePO {
 
     private By addToOrder = By.id("btn_customize_item_add_to_order");
+    private By size = By.xpath("//select[@id='ddl_customize_item_item1_size']");
+
+    public Element size() throws IOException, InterruptedException {
+        return $(size);
+    }
 
     public Element addToOrder() throws IOException, InterruptedException {
         return $(addToOrder);
