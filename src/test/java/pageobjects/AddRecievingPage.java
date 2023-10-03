89fe6ec9c245348eef065b495c6f1f1ade1b4bea
RahulJain95 <jnrahul.95@gmail.com>
2023-10-03 10:09:15 +0530

    fix regression

--- src/test/java/pageobjects/AddRecievingPage.java
+++ src/test/java/pageobjects/AddRecievingPage.java
@@ -14,7 +14,7 @@ public class AddRecievingPage extends BasePO {
     private By tax = By.id("txt_BO_IN_REC_ADDEDT_Tax");
     private By freight = By.id("txt_BO_IN_REC_ADDEDT_Freight");
     private By others = By.id("txt_BO_IN_REC_ADDEDT_OtherCharges");
-    private By recievingTxtPopup = By.xpath("//span[@id='div_BO_IN_VIT_ADDEDT_DIALOG_Header_Title']");
+    private By recievingTxtPopup = By.xpath("//span[@id='div_BO_IN_REC_ADDEDT_DIALOG_Header_Title']");
     private By save = By.id("btn_BO_IN_VIT_ADDEDT_Save");
 
     public Element others() throws IOException, InterruptedException {
