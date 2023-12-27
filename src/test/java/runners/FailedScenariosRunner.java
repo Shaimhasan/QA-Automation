02c6cae0a3c9bb89dd035fdeac0743451e60b6fa
RahulJain95 <jnrahul.95@gmail.com>
2023-12-27 09:51:29 +0530

    fix failed files

--- src/test/java/runners/FailedScenariosRunner.java
+++ src/test/java/runners/FailedScenariosRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         plugin = {"core.AutoEngineFormatter",
-                "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "json:target/cucumber-reports/runReport.json",
                 "rerun:failScenarios/rerun.txt"},
         features = {"@failScenarios/rerun.txt"}
